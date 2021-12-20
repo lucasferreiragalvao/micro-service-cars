3a127bdd19e8e0838358f85b4e79dc288e72de75
Lucas Galvão <lucasferreiragalvaoti@gmail.com>
2021-12-20 19:42:16 -0300

    feat: init project

---
+++ src/main/java/com/unifacef/work/groupone/microservicecars/MicroserviceCarsApplication.java
@@ -0,0 +1,13 @@
+package com.unifacef.work.groupone.microservicecars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceCarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceCarsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unifacef/work/groupone/microservicecars/MicroserviceCarsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unifacef.work.groupone.microservicecars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceCarsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
