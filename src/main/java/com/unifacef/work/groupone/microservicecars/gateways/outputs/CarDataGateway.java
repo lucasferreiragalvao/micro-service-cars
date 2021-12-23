03a6c44af9bb51bea4f307129f7458847ea53287
Lucas Galvão <lucasferreiragalvaoti@gmail.com>
2021-12-23 15:41:14 -0300

    feat: add endpoint find car

--- src/main/java/com/unifacef/work/groupone/microservicecars/gateways/outputs/CarDataGateway.java
+++ src/main/java/com/unifacef/work/groupone/microservicecars/gateways/outputs/CarDataGateway.java
@@ -4,6 +4,10 @@ import com.unifacef.work.groupone.microservicecars.domains.Car;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import java.util.Optional;
+
 public interface CarDataGateway {
+
+    Optional<Car> findByCode(String code);
     Page<Car> findByPage(Pageable pageable);
 }
