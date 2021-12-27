724ee2c0552b058de447d9d6c5bae349b8f2afaf
Lucas Galvão <lucasferreiragalvaoti@gmail.com>
2021-12-27 15:44:25 -0300

    feat: add endpoint update car

--- src/main/java/com/unifacef/work/groupone/microservicecars/gateways/inputs/http/CarsController.java
+++ src/main/java/com/unifacef/work/groupone/microservicecars/gateways/inputs/http/CarsController.java
@@ -1,11 +1,13 @@
 package com.unifacef.work.groupone.microservicecars.gateways.inputs.http;
 
 import com.unifacef.work.groupone.microservicecars.gateways.inputs.http.requests.CreateCarRequest;
+import com.unifacef.work.groupone.microservicecars.gateways.inputs.http.requests.UpdateCarRequest;
 import com.unifacef.work.groupone.microservicecars.gateways.inputs.http.responses.CarResponse;
 import com.unifacef.work.groupone.microservicecars.gateways.inputs.http.responses.ListResponse;
 import com.unifacef.work.groupone.microservicecars.usecases.CreateCar;
 import com.unifacef.work.groupone.microservicecars.usecases.FindByCarCode;
 import com.unifacef.work.groupone.microservicecars.usecases.FindCars;
+import com.unifacef.work.groupone.microservicecars.usecases.UpdateCar;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -17,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 
 @RestController
 @RequestMapping(value = "/api/v1/cars")
@@ -24,12 +27,19 @@ import org.springframework.web.bind.annotation.PostMapping;
 public class CarsController {
 
     private final CreateCar createCar;
+    private final UpdateCar updateCar;
     private final FindCars findCars;
     private final FindByCarCode findByCarCode;
+
 
     @PostMapping
     public CarResponse create(@RequestBody @Validated final CreateCarRequest request){
         return new CarResponse(createCar.execute(request.toDomain()));
+    }
+
+    @PutMapping(path = "/{code}")
+    public CarResponse update(@PathVariable final String code, @RequestBody @Validated final UpdateCarRequest request){
+        return new CarResponse(updateCar.execute(request.toDomain(code)));
     }
 
     @GetMapping(path = "/{code}")
